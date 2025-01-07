46c28ddf44f75e9ca5ce2ae7c504b6ecaeef7a20
SeenthilvelKaleeswaran <devfallous@gmail.com>
2025-01-07 23:35:04 +0530

    updt(create-discussion) : updated create discussion form with more fields

--- packages/frontend/src/components/ui/SelectOption.jsx
+++ packages/frontend/src/components/ui/SelectOption.jsx
@@ -21,9 +21,21 @@ const Select = ({
 
   const outsideClikRef = useOutsideClickListener(closeMenu);
 
+  const isGroupped =
+    Array.isArray(options) && options?.length > 0 && !!options[0]?.options;
+
   return (
     <SelectContext.Provider
-      value={{id, value, onChange, isOpen, toggleOpen, closeMenu, options }}
+      value={{
+        id,
+        value,
+        onChange,
+        isOpen,
+        toggleOpen,
+        closeMenu,
+        options,
+        isGroupped,
+      }}
     >
       <div className={`relative w-full ${className}`} ref={outsideClikRef}>
         <RenderSpace condition={label}>
@@ -41,10 +53,17 @@ const Select = ({
 };
 
 const Trigger = ({ children, className, disabled }) => {
-  const { isOpen, toggleOpen, value, options } = useContext(SelectContext);
+  const { isOpen, toggleOpen, value, options, isGroupped } =
+    useContext(SelectContext);
 
-  // Find the label of the selected value
-  const selectedOption = options?.find((option) => option.value === value);
+  const selectedOption = isGroupped
+    ? options?.find((item) => {
+        return item?.options?.find((option) => option.value === value);
+      })
+    : options?.find((item) => {
+        return item.value === value;
+      });
+
   const displayValue = selectedOption ? selectedOption.label : children;
 
   return (
@@ -82,7 +101,7 @@ const Option = ({ children, value, disabled }) => {
 
   const handleSelect = () => {
     if (!disabled) {
-      onChange(id,value);
+      onChange(id, value);
       closeMenu();
     }
   };
@@ -112,7 +131,7 @@ const Group = ({ label, separator, children }) => {
         <hr className="border-t border-gray-500 px-2 " />
       </RenderSpace>
       <RenderSpace condition={label}>
-        <div className="px-4 py-1 text-sm text-left font-semibold text-gray-600">
+        <div className="py-1 text-sm text-left font-semibold text-gray-600">
           {label}
         </div>
       </RenderSpace>
@@ -123,15 +142,31 @@ const Group = ({ label, separator, children }) => {
 };
 
 const DropdownSelect = ({ options, placeholder, ...rest }) => {
+  const isGrouped = Array.isArray(options) && options[0]?.options;
+
+  const renderOptions = (options) => {
+    return options.map((option) => (
+      <Option key={option.value} value={option.value}>
+        {option.label}
+      </Option>
+    ));
+  };
+
   return (
     <Select {...rest} options={options}>
       <Trigger>{placeholder}</Trigger>
       <Options>
-        {options.map((option) => (
-          <Option key={option.value} value={option.value}>
-            {option.label}
-          </Option>
-        ))}
+        {isGrouped
+          ? options.map((group) => (
+              <Group
+                key={group.id}
+                label={group.label}
+                separator={group.separator}
+              >
+                {renderOptions(group.options)}
+              </Group>
+            ))
+          : renderOptions(options)}
       </Options>
     </Select>
   );
--- packages/frontend/src/screens/CreateDiscussion.jsx
+++ packages/frontend/src/screens/CreateDiscussion.jsx
@@ -2,17 +2,54 @@ import { useMutation } from "react-query";
 import { createDiscussion } from "../utils/api-call";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Section } from "../components/screens";
+import {
+  Group,
+  Options,
+  Option,
+  Select,
+  TextInput,
+  Trigger,
+  DropdownSelect,
+  Checkbox,
+  Button,
+} from "../components/ui";
+import { AI_MEMBERS_FORM_DATA, FORM_METADATA } from "../constants";
+import { RenderSpace } from "../components/shared";
 
 export const CreateDiscussion = () => {
+  const [form, setForm] = useState("details");
   const [discussionDetails, setDiscussionDetails] = useState({
     topic: "Online Class vs Offline Class",
-    isTopicAiGenerated: false, // New field
-    aiModelsCount: 3,
-    noOFUsers: 1,
-    discussionLength: 5, // Discussion duration in minutes
-    conclusionBy: "Random",
-    conclusionPoints: 1, // Default number of conclusion points
-    micAccessWaitTime: 2, // Default wait time for mic access in seconds
+    topicSetting: "admin",
+    aiParticipants: [],
+    participants: [],
+    otherParticipants: [],
+    discussionMode: "selection",
+    discussionLength: 5,
+    discussionLengthSetting: "fixed",
+    pointsSetting: "noLimit",
+    minPoints: 0,
+    maxPoints: 0,
+    pointsPerParticipant: 0,
+    conclusionBy: "both",
+    conclusionMode: "selection",
+    conclusionLength: 0,
+    conclusionLengthSetting: "fixed",
+    aiSpeechMode: "selection",
+    aiSpeaksAtFrequency: 0,
+    accessConversation: false,
+    accessOthersConversation: false,
+    accessFeedback: false,
+    accessOthersFeedback: false,
+    accessParticipantConversation: false,
+    accessParticipantFeedback: false,
+    micAccessWaitTime: 2,
+    createdBy: "",
+    rounds: "single",
+    status: "notStarted",
+    sessionStartTime: null,
+    sessionEndTime: null,
   });
 
   const navigate = useNavigate();
@@ -29,11 +66,17 @@ export const CreateDiscussion = () => {
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     setDiscussionDetails((prev) => {
+      if (type === "checkbox") {
+        return {
+          ...prev,
+          [name]: checked,
+        };
+      }
       if (name === "isTopicAiGenerated") {
         return {
           ...prev,
           isTopicAiGenerated: checked,
-          topic: checked ? "" : "Online vs Offline Class", // Clear topic if AI-generated
+          topic: checked ? "" : "Online vs Offline Class",
         };
       }
       return {
@@ -43,257 +86,157 @@ export const CreateDiscussion = () => {
     });
   };
 
+  const handleDropdownChange = (name, value) => {
+    setDiscussionDetails((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     mutate(discussionDetails);
   };
 
-  // Determine valid options for "conclusionBy"
-  const getOptions = () => {
-    if (
-      discussionDetails.conclusionPoints > 1 &&
-      discussionDetails.noOFUsers > 1
-    ) {
-      return [
-        { value: "Users", label: "👥 Users (AI will take a break)" },
-        { value: "AI", label: "🤖 AI Spotlight (Only AI speaks)" },
-        { value: "Random", label: "🎲 Open Mic (Anyone can jump in)" },
-      ];
-    }
+  const isDisabled = (field) => {
+    const { id, disableCondition } = field;
 
-    if (
-      discussionDetails.conclusionPoints > 1 &&
-      discussionDetails.noOFUsers === 1
-    ) {
-      return [
-        { value: "AI", label: "🤖 AI Finale (AI wraps up)" },
-        { value: "Random", label: "🌈 Free Flow (Open participation)" },
-      ];
+    if (disableCondition && Array.isArray(disableCondition)) {
+      return disableCondition.every((item) => {
+        const { value } = item;
+        if (Array.isArray(value)) {
+          return value.includes(discussionDetails[item.id]);
+        }
+        return discussionDetails[item.id] === value;
+      });
     }
-
-    if (
-      discussionDetails.conclusionPoints === 1 &&
-      discussionDetails.noOFUsers === 1
-    ) {
-      return [
-        { value: "You", label: "👤 Your Stage" },
-        { value: "AI", label: "🤖 AI Synthesizes" },
-        { value: "Random", label: "🌐 Open Arena" },
-      ];
-    }
-    if (
-      discussionDetails.conclusionPoints === 1 &&
-      discussionDetails.noOFUsers > 1
-    )
-      return [
-        { value: "Users", label: "👥 Collective Wisdom" },
-        { value: "AI", label: "🧠 AI Insight Mode" },
-        { value: "Random", label: "🎲 Open Access Mode" },
-      ];
-
-    return [];
+    return false;
   };
-  const conclusionOptions = getOptions();
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-blue-500 to-indigo-600">
       <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-3xl space-y-8">
-        <h1 className="text-4xl font-semibold text-center text-gray-800">
-          Start Your Group Discussion
-        </h1>
         <form onSubmit={handleSubmit} className="space-y-6">
-          {/* Basic Details */}
-          <section className="border rounded-md border-black p-4">
-            <h2 className="text-2xl font-medium text-gray-700">
-              Basic Details
-            </h2>
+          <div className="flex justify-between gap-4">
+            <h1 className="text-4xl font-semibold text-center text-gray-800">
+              Start Your Group Discussion
+            </h1>
+            <div className="flex gap-4">
+              <RenderSpace condition={form !== "details"}>
+                <Button
+                  label="Back"
+                  variant="secondary"
+                  onClick={() => setForm("details")}
+                />
+              </RenderSpace>
 
+              <RenderSpace condition={form !== "members"}>
+                <Button
+                  label="Next"
+                  variant="primary"
+                  onClick={() => setForm("members")}
+                />
+              </RenderSpace>
+              <RenderSpace condition={form === "members"}>
+                <Button label={isLoading ? "Creating..." : "Create"} variant="success" />
+              </RenderSpace>
+            </div>
+          </div>
+          {form === "details" ? (
+            FORM_METADATA?.map((section, index) => (
+              <Section
+                key={index}
+                title={section.title}
+                description={section.description}
+              >
+                <div className="grid grid-cols-2 gap-4">
+                  {section.fields.map((field, idx) => {
+                    console.log({ aaa: isDisabled(field), field: field?.id });
+                    if (field.component === "TextInput") {
+                      return (
+                        <TextInput
+                          key={idx}
+                          value={discussionDetails[field.id]}
+                          {...field}
+                          onChange={handleChange}
+                          disabled={isDisabled(field)}
+                        />
+                      );
+                    } else if (field.component === "DropdownSelect") {
+                      return (
+                        <DropdownSelect
+                          key={idx}
+                          value={discussionDetails[field.id]}
+                          {...field}
+                          onChange={handleDropdownChange}
+                          disabled={isDisabled(field)}
+                        />
+                      );
+                    } else if (field.component === "Checkbox") {
+                      return (
+                        <Checkbox
+                          key={idx}
+                          {...field}
+                          onChange={handleChange}
+                          disabled={isDisabled(field)}
+                        />
+                      );
+                    }
+                    return null; // Fallback for any unhandled field types
+                  })}
+                </div>
+              </Section>
+            ))
+          ) : (
             <div>
-              <label
-                htmlFor="topic"
-                className="block text-lg font-medium text-gray-700 mb-2"
-              >
-                Discussion Topic
-              </label>
-              <input
-                type="text"
-                id="topic"
-                name="topic"
-                value={discussionDetails.topic}
-                onChange={handleChange}
-                disabled={discussionDetails.isTopicAiGenerated}
-                placeholder={
-                  discussionDetails.isTopicAiGenerated
-                    ? "Topic will be generated by AI"
-                    : "Enter your topic"
-                }
-                className={`w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 ${
-                  discussionDetails.isTopicAiGenerated
-                    ? "bg-gray-100 cursor-not-allowed"
-                    : "focus:ring-blue-500"
-                } transition duration-200 ease-in-out shadow-sm`}
-              />
-            </div>
-            <div className="mb-4 flex items-center gap-4">
-              <label
-                htmlFor="isTopicAiGenerated"
-                className="text-lg font-medium text-gray-700"
-              >
-                Generate Topic Using AI
-              </label>
-              <input
-                type="checkbox"
-                id="isTopicAiGenerated"
-                name="isTopicAiGenerated"
-                checked={discussionDetails.isTopicAiGenerated}
-                onChange={handleChange}
-                className="w-6 h-6"
-              />
-            </div>
-          </section>
-
-          {/* Other Settings */}
-          <section className="border p-4 border-black rounded-md">
-            <h2 className="text-2xl font-medium text-gray-700">
-              Users Settings
-            </h2>
-            <div className="grid grid-cols-2 gap-4">
-              <div>
-                <label
-                  htmlFor="aiModelsCount"
-                  className="block text-lg font-medium text-gray-700 mb-2"
+              {AI_MEMBERS_FORM_DATA?.map((section, index) => (
+                <Section
+                  key={index}
+                  title={section.title}
+                  description={section.description}
                 >
-                  Number of AI Members
-                </label>
-                <input
-                  type="number"
-                  id="aiModelsCount"
-                  name="aiModelsCount"
-                  value={discussionDetails.aiModelsCount}
-                  onChange={handleChange}
-                  className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out shadow-sm"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="noOFUsers"
-                  className="block text-lg font-medium text-gray-700 mb-2"
-                >
-                  Number of Users
-                </label>
-                <input
-                  type="number"
-                  id="noOFUsers"
-                  name="noOFUsers"
-                  value={discussionDetails.noOFUsers}
-                  onChange={handleChange}
-                  disabled
-                  className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out shadow-sm"
-                />
-              </div>
+                  <div className="grid grid-cols-2 gap-4">
+                    {section.fields.map((field, idx) => {
+                      console.log({ aaa: isDisabled(field), field: field?.id });
+                      if (field.component === "TextInput") {
+                        return (
+                          <TextInput
+                            key={idx}
+                            value={discussionDetails[field.id]}
+                            {...field}
+                            onChange={handleChange}
+                            disabled={isDisabled(field)}
+                          />
+                        );
+                      } else if (field.component === "DropdownSelect") {
+                        return (
+                          <DropdownSelect
+                            key={idx}
+                            value={discussionDetails[field.id]}
+                            {...field}
+                            onChange={handleDropdownChange}
+                            disabled={isDisabled(field)}
+                          />
+                        );
+                      } else if (field.component === "Checkbox") {
+                        return (
+                          <Checkbox
+                            key={idx}
+                            {...field}
+                            onChange={handleChange}
+                            disabled={isDisabled(field)}
+                          />
+                        );
+                      }
+                      return null; // Fallback for any unhandled field types
+                    })}
+                  </div>
+                </Section>
+              ))}
             </div>
-          </section>
-          <section className="border p-2 border-black rounded-md">
-            <h2 className="text-2xl font-medium text-gray-700">
-              Conversation Settings
-            </h2>
-            <p className="text-sm text-gray-500 mb-4">
-              Choosethe settings for conversation.
-            </p>
-            <div className="grid grid-cols-2 gap-4">
-              <div>
-                <label
-                  htmlFor="discussionLength"
-                  className="block text-lg font-medium text-gray-700 mb-2"
-                >
-                  Discussion Length (Minutes)
-                </label>
-                <input
-                  type="number"
-                  id="discussionLength"
-                  name="discussionLength"
-                  value={discussionDetails.discussionLength}
-                  onChange={handleChange}
-                  className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out shadow-sm"
-                />
-              </div>
-            </div>
-          </section>
-
-          {/* Section 2: Conclusion Settings */}
-          <section className="border p-2 border-black rounded-md">
-            <h2 className="text-2xl font-medium text-gray-700">
-              Conclusion Settings
-            </h2>
-            <p className="text-sm text-gray-500 mb-4">
-              Choose who will summarize the discussion and the number of points
-              to summarize.
-            </p>
-            <div className="grid grid-cols-2 gap-4">
-              <div>
-                <label
-                  htmlFor="conclusionPoints"
-                  className="block text-lg font-medium text-gray-700 mb-2"
-                >
-                  Number of Conclusion Points
-                </label>
-                <input
-                  type="number"
-                  id="conclusionPoints"
-                  name="conclusionPoints"
-                  value={discussionDetails.conclusionPoints}
-                  onChange={handleChange}
-                  className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out shadow-sm"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="conclusionBy"
-                  className="block text-lg font-medium text-gray-700 mb-2"
-                >
-                  Who Summarizes?
-                </label>
-                <select
-                  id="conclusionBy"
-                  name="conclusionBy"
-                  value={discussionDetails.conclusionBy}
-                  onChange={handleChange}
-                  className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out shadow-sm"
-                >
-                  {conclusionOptions.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </div>
-          </section>
-
-          {/* Section 3: Mic Settings */}
-          <section className="border p-2 border-black rounded-md">
-            <h2 className="text-2xl font-medium text-gray-700">Mic Settings</h2>
-            <p className="text-sm text-gray-500 mb-4">
-              Configure how long the system waits before granting access to the
-              mic.
-            </p>
-            <div>
-              <label
-                htmlFor="micAccessWaitTime"
-                className="block text-lg font-medium text-gray-700 mb-2"
-              >
-                Mic Access Wait Time (Seconds)
-              </label>
-              <input
-                type="number"
-                id="micAccessWaitTime"
-                name="micAccessWaitTime"
-                value={discussionDetails.micAccessWaitTime}
-                onChange={handleChange}
-                className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out shadow-sm"
-              />
-            </div>
-          </section>
+          )}
 
           <button
             type="submit"
